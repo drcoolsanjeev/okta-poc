91676d0a8f4a65914c81aefd89b20eb0286fcf51
sanjeev kumar <sanjeevkumar201418@gmail.com>
2021-04-22 14:37:14 +0530

    first setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
 
+import Navbar from './components/Layout/Navbar'
+import Home from './pages/Home';
+import Staff from './pages/Staff';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Router>
+    <div className="App">      
+    <Navbar/>
+    <div class="container">
+      <Route path="/" exact={true} component={Home}/>
+      <Route path="/staff" exact={true} component={Staff}/>
+      </div>
     </div>
+  </Router>
   );
 }
 
